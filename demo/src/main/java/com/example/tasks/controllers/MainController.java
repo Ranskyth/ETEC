11c6520ae5e00e4021f14250ae84ab8e2b7eeb0f
gabriel <ga.lima2205@gmail.com>
2025-08-08 20:08:39 -0300

    up

--- demo/src/main/java/com/example/tasks/controllers/MainController.java
+++ demo/src/main/java/com/example/tasks/controllers/MainController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controllers;
+package com.example.tasks.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- demo/src/main/java/com/example/tasks/controllers/TaskController.java
+++ demo/src/main/java/com/example/tasks/controllers/TaskController.java
@@ -1,15 +1,14 @@
-package com.example.demo.controllers;
+package com.example.tasks.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/api")
 public class TaskController{
     
     @GetMapping("/task")
     public void listTask(){
-        System.out.println("");
+        System.out.println("oiii");
     }
 }
